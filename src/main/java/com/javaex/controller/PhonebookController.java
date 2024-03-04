55997d2c639b68dd9ec0bdfafadd844386e0923e
seed-yoo <lukdown10@naver.com>
2024-03-04 18:44:27 +0900

    2024/03/04

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.javaex.dao.PhonebookDao;
 import com.javaex.service.PhonebookService;
 import com.javaex.vo.PersonVo;
 
